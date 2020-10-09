2f90e0bb55df36cd29cf3bcd1b4c59108caa6f59
EnYu <57358478+heehee812@users.noreply.github.com>
2020-10-09 23:51:33 +0800

    Create/block (#5)
    * Add a select_block function
    
    * Clarify the end and game over
    
    * Add a class Block

--- teris.cpp
+++ teris.cpp
@@ -1,73 +1,186 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*------------shared variable--------------*/
+typedef vector<int> Col;
+int isend= 0;
 /*------------shared function--------------*/
 void read_file(char *, char *, char *, fstream *);
 int string_to_int(const char *);
-void game_over();
+void game_over(int);
 
 /*-----------------class-------------------*/
 class GameBoard{
-    private:
+    protected:
         int row, col;
         int **gameboard;
     public:
         GameBoard(int n, int m): row(n), col(m){
-            gameboard= new_game_board();
+            gameboard= new_gameboard(row, col);
         }
-        int** new_game_board(){
+        GameBoard(){}
+        int** new_gameboard(int n, int m){
             int ele= 0;
-            int **element= new int*[row];
-            for(int i=0; i<row; i++){
-                element[i]= new int[col];
+            int **element= new int*[n];
+            for(int i=0; i<n; i++){
+                element[i]= new int[m];
             }
-            for(int i=0; i<row; i++)
-                for(int j= 0; j<col; j++){
-                    element[i][j]= ele++;
+            for(int i=0; i<n; i++)
+                for(int j= 0; j<m; j++){
+                    element[i][j]= 0;
                 }
             return element;
         }
-        void print_game_board(){
-            for(int i=0; i<row; i++)
+
+        void print_gameboard(){
+            for(int i=0; i<row; i++){
                 for(int j= 0; j<col; j++){
                     cout<<gameboard[i][j];
                 }
-        }
-        void seclect_block(int shape){
-            switch(shape){
-                case(73):{ //I
-                    cout<<"select I"<<endl;
-                    break;
-                }
-                case(74):{ //J
-                    cout<<"select J"<<endl;
-                    break;
-                }
-                case(76):{ //L
-                    cout<<"select L"<<endl;
-                    break;
-                } 
-                case(79):{ //O
-                    cout<<"select O"<<endl;
-                    break;
-                }
-                case(83):{ //S
-                    cout<<"select S"<<endl;
-                    break;
-                }
-                case(84):{ //T
-                    cout<<"select T"<<endl;
-                    break;
-                }
-                case(90):{ //Z
-                    cout<<"select Z"<<endl;
-                    break;
-                }
-                default:
-                    game_over();
+                cout<<endl;
             }
         }
 };
+
+class Table{
+    private:
+        int row, col;
+        vector<Col> nonzerotable, bombtable;
+    public:
+        Table(int n, int m): row(n), col(m){
+            nonzerotable.reserve(col);
+            for(int i= 0; i<col; i++){
+                nonzerotable[i].emplace_back(0);
+            }
+            for (int i= 0; i<col; i++)
+            {
+                for (auto j: nonzerotable[i])
+                    std::cout << j << " ";
+                std::cout << '\n';
+            }
+        }
+        // int get_bottom(int pos1){}
+  
+};
+
+class Block: public GameBoard{
+    private:
+        int pos1, pos2, bottom1;
+        char *kind;
+    public:
+        Block(int x1, int x2, char *shape): pos1(x1), pos2(x2), kind(shape){
+        }
+        void check_is_hit(){
+            int dir= kind[1]-'0';
+            switch(kind[0]){
+                case(73):{ //I
+                    switch(dir){
+                        case(1):{
+                            break;
+                        }
+                        case(2):{
+                            break;
+                        }
+                        default:
+                            game_over(1);
+                    }
+                    break;
+                }
+                case(74):{ //J
+                    switch(dir){
+                        case(1):{
+                            break;
+                        }
+                        case(2):{
+                            break;
+                        }
+                        case(3):{
+                            break;
+                        }
+                        case(4):{
+                            break;
+                        }
+                        default:
+                            game_over(1);
+                    }
+                    break;
+                }
+                case(76):{ //L
+                    switch(dir){
+                        case(1):{
+                            break;
+                        }
+                        case(2):{
+                            break;
+                        }
+                        case(3):{
+                            break;
+                        }
+                        case(4):{
+                            break;
+                        }
+                        default:
+                            game_over(1);
+                    }
+                    break;
+                } 
+                case(79):{ //O
+                    break;
+                }
+                case(83):{ //S
+                    switch(dir){
+                        case(1):{
+                            break;
+                        }
+                        case(2):{
+                            break;
+                        }
+                        default:
+                            game_over(1);
+                    }
+                    break;
+                }
+                case(84):{ //T
+                    switch(dir){
+                        case(1):{
+                            break;
+                        }
+                        case(2):{
+                            break;
+                        }
+                        case(3):{
+                            break;
+                        }
+                        case(4):{
+                            break;
+                        }
+                        default:
+                            game_over(1);
+                    }
+                    break;
+                }
+                case(90):{ //Z
+                    switch(dir){
+                        case(1):{
+                            break;
+                        }
+                        case(2):{
+                            break;
+                        }
+                        default:
+                            game_over(1);
+                    }
+                    break;
+                }
+                default:{
+                    game_over(1);
+                }
+            }
+        }
+        void assign_to_gameboard(){}
+};
+
+
 
 /*---------------main function-----------------*/
 int main(){
@@ -83,14 +196,17 @@ int main(){
     int rowi= string_to_int(row);
     int coli= string_to_int(col);
     GameBoard gameboard(rowi, coli);
-    // gameboard.print_game_board();
+    Table table(rowi, coli);
+    // gameboard.print_gameboard();
 
     //load in a test case
     while(!ifile.eof()){
+        if(isend)
+            break;
         read_file(shape, pos1, pos2, &ifile);
         int pos1i= string_to_int(pos1);
         int pos2i= string_to_int(pos2);
-        gameboard.seclect_block(shape[0]);
+        Block block(pos1i, pos2i, shape);
     }
     ifile.close();
     return 0;
@@ -100,8 +216,8 @@ int main(){
 /*------------shared function--------------*/
 void read_file(char *shape, char *pos1, char *pos2, fstream *ifile){
     (*ifile).getline(shape, sizeof(shape), ' ');
-    if(shape[0]== 69);
-        // game_over();
+    if(shape[0]== 69)
+        game_over(0);
     else{
         (*ifile).getline(pos1, sizeof(pos1), ' ');
         (*ifile).getline(pos2, sizeof(pos2), '\n');
@@ -120,6 +236,10 @@ int string_to_int(const char *word){
     return number;
 }
 
-void game_over(){
-    cout<<"---------------GAME OVER------------------"<<endl;
+void game_over(int i){
+    if(i)
+        cout<<"---------------GAME OVER------------------"<<endl;
+    else
+        cout<<"---------------End------------------"<<endl;
+    isend= 1;
 }
